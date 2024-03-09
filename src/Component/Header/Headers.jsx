31d1abfd2d035329df03603e019e02362597f11b
Ayibiowu Gbenga <ayibiowug@gmail.com>
2024-03-09 15:34:44 +0100

    added other files

---
+++ src/Component/Header/Headers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Headers() {
+  return (
+    <div>
+      <h1>Header is back</h1>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default Headers;
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Headers from "../Component/Header/Headers";
+
+function Home() {
+    return (
+        <Headers/>
+    )
+}
+
+export default Home
